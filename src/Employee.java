02c885b4799af6aaa22a259f9dbab6bf71a5bc04
KatrineThoft <s153763@student.dtu.dk>
2017-03-27 14:22:07 +0200

    First draft of classes Employee and Project Manager

---
+++ src/Employee.java
@@ -0,0 +1,29 @@
+/**
+ * Created by katrinethoft on 27/03/17.
+ */
+public class Employee {
+  /*  private String employeeName;
+    private List<Activiy> ongoingActivities;
+
+    public Employee(String employeeName, List<Activity> ongoingActivities){
+        this.employeeName = employeeName;
+        this.ongoingActivities = ongoingActivities;
+
+    }
+
+    public boolean checkAbsense(String employeeName){
+        //Do stuff
+
+        return false;
+    }
+
+
+    public void timeRegistering(double hours, String employeeName){
+        //Do stuff
+    }
+
+    public void editRegisteredTime(double hours, String employeeName, Date day){
+        //Do stuff
+    }
+    */
+}
---
+++ src/ProjectManager.java
@@ -0,0 +1,36 @@
+/**
+ * Created by katrinethoft on 27/03/17.
+ */
+/*public class ProjectManager extends Employee  {
+    private Employee manager;
+
+    public ProjectManager(Employee manager, String employeeName, List<Activities> ongoingActivities){
+        super(employeeName, ongoingActivities);
+        this.manager = manager;
+    }
+
+
+    public List<Activity, Employee> delegateActivities(List<Employee> employees, List<Activity> activities){
+        List<Activity, Employee> list = new List<Activity, Employee>;
+        //Do stuff
+        return list;
+    }
+
+    public void projectMeeting(String projectID){
+        //Do stuff
+    }
+
+    public Employee findSubstitute(Activity act){
+        //Do stuff
+        return null;
+    }
+
+    public void informClient(String projectID, String message){
+
+    }
+
+    public void delayProject(String projectID){
+
+    }
+}
+*/
