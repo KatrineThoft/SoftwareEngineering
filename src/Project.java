5088ed0c04413edfbca72ee27a289d6cf6b550e7
Daniel Hildebrand <s154410@student.dtu.dk>
2017-03-27 14:32:57 +0200

    hej

---
+++ src/Project.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Daniel Hildebrand on 27-03-2017.
+ */
+public class Project {
+
+    public static void main(String[] args) {
+        public String ProjectID(String "id"){
+           this.id= "id" ;
+        }
+
+    public String getProjectID(){
+           return ProjectID();
+        }
+        public String ProjectName(String){}
+        int EndDate= 5;
+
+    }
+
+}
