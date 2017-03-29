c177aeaaecf81b9c554afc082c198fff93fbe08b
EmilieKvist <s153971@student.dtu.dk>
2017-03-29 12:44:50 +0200

    Client and Activity class igen (activity hedder Assignment...)

--- src/Assignment.java
+++ src/Assignment.java
@@ -2,5 +2,20 @@
  * Created by EmilieKvist on 29-03-2017.
  */
 public class Assignment {
-    //ignore this class
+    public String activityName;
+    public double estimatedTimeUse;
+    public double timeUsed;
+
+    public Assignment(String activityName, double estimatedTimeUse){
+        this.activityName = activityName;
+        this.estimatedTimeUse = estimatedTimeUse;
+    }
+
+    public void setTimeUsed(double timeUsed){
+        this.timeUsed = timeUsed;
+    }
+
+    public double remainingTime(){
+        return estimatedTimeUse - timeUsed;
+    }
 }
