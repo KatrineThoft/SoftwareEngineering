6d1ca98e5b6ef22dd08ffa65fce6ec9fd4c57ae1
KatrineThoft <s153763@student.dtu.dk>
2017-05-05 14:49:30 +0200

    GUI Client implementeret og employee implementeret til og med EmpOpt. Påbegyndt Project Manager.

---
+++ src/GUI/AbsenceState.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by katrinethoft on 05/05/17.
+ */
+public class AbsenceState  extends Stage {
+
+    public AbsenceState(CompanyDriver companyDriver){
+
+    }
+
+}
